554bf0148d14b818faf41e2792dff0f0b388f403
Peter Peng <yser.pc@gmail.com>
2022-12-01 13:58:23 -0500

    restructured files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BoardComponent } from './board/board.component';
-import { BlockComponent } from './block/block.component';
+import { BoardComponent } from './component/board/board.component';
+import { BlockComponent } from './component/block/block.component';
 import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogModule } from '@angular/material/dialog';
-import { StartingDialogComponent } from './starting-dialog/starting-dialog.component';
+import { StartingDialogComponent } from './dialog/starting-dialog/starting-dialog.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
-import { WaitingDialogComponent } from './waiting-dialog/waiting-dialog.component';
-import { JoiningDialogComponent } from './joining-dialog/joining-dialog.component';
-import { WinningDialogComponent } from './winning-dialog/winning-dialog.component';
+import { WaitingDialogComponent } from './dialog/waiting-dialog/waiting-dialog.component';
+import { JoiningDialogComponent } from './component/block/joining-dialog/joining-dialog.component';
+import { WinningDialogComponent } from './dialog/winning-dialog/winning-dialog.component';
 
 @NgModule({
   declarations: [
