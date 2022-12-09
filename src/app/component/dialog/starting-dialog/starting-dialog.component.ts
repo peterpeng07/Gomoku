620a0f6f99d6f59d9afee26edcbbaaac274d3bbd
Peter Peng <yser.pc@gmail.com>
2022-12-09 14:20:39 -0500

    store name in local storage

--- src/app/component/dialog/starting-dialog/starting-dialog.component.ts
+++ src/app/component/dialog/starting-dialog/starting-dialog.component.ts
@@ -11,6 +11,14 @@ export class StartingDialogComponent {
   constructor(
     public dialogRef: MatDialogRef<StartingDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public name: string,
-  ) { }
+  ) {
+    const player = localStorage.getItem('playerName');
+    if (player)
+      this.name = player;
+  }
+
+  nameChange(): void {
+    localStorage.setItem('playerName', this.name);
+  }
 
 }
